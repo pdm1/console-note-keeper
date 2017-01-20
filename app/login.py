7ef669df4911e395c342119059d03d7958d0722d
Patrick Matthies <coding.newb80@gmail.com>
2017-01-20 22:35:39 +0000

    Started basic login... don't ever use this in a real thing

---
+++ app/login.py
@@ -0,0 +1,35 @@
+class Login:
+    """This is for basic concept... for the love of god,
+    
+    do not use this in a real application
+    """
+    def __init__(self):
+        self.accounts = {}
+        
+    def create_account(self, user_name, user_pw, user_pw_repeat):
+        if self._password_check(user_pw, user_pw_repeat): #Check if PW is valid
+            if user_name not in self.accounts:
+                self.accounts[user_name] = user_pw
+    
+    def _password_check(self, user_pw, user_pw_repeat):
+        if user_pw != user_pw_repeat:
+            print("Passwords do not match, retry")
+            return False
+        if len(user_pw) < 7:
+            print("Password is too short. Required, at least 7 characters with one being a symbol")
+            return False
+        symbols = ['!','@','#','$','%','^','&','*','(',')']
+        if not any(x in user_pw for x in symbols):
+            print("Password must contain at least one symbol (Number shift chars)")
+            return False
+        if user_pw == user_pw_repeat:
+            return True
+        
+            
+
+me = Login()
+
+me.create_account("Bobby", "kitty12", "kitty12")
+print me.accounts
+
+        
