a30a701f3c67a27de60b73fc40ca96ada55dd59a
Patrick Matthies <coding.newb80@gmail.com>
2017-01-20 23:14:33 +0000

    updating login

--- app/main.py
+++ app/main.py
@@ -15,24 +15,30 @@ while select != 0:
         "5. Delete all completed" + "\n" +
         "6. View all Todo" + "\n" +
         "0. Quit")
-    select = int(raw_input("choose option "))
-    if select == 1:
-        select = raw_input("Add Todo:  ")
-        my_todos.add_todo(select)
-    if select == 2:
-        select = raw_input("Update Todo at number:  ")
-        my_todos.update_todo(select)
-    if select == 3:
-        select = raw_input("Delete Todo at number:  ")
-        my_todos.delete_todo(select)
-    if select == 4:
-        select = raw_input("Mark as complete at number:  ")
-        my_todos.complete_todo(select)
-    if select == 5:
-        select = raw_input("Delete all complete? 1. Yes 2. No  ")
-        my_todos.delete_complete_todo()
-    if select == 6:
-        pprint(my_todos.show_todo())
-    if select == 0:
-        quit()
+    try:
+        select = int(raw_input("choose option "))
+        if select == 1:
+            select = raw_input("Add Todo:  ")
+            my_todos.add_todo(select)
+        if select == 2:
+            select = raw_input("Update Todo at number:  ")
+            my_todos.update_todo(select)
+        if select == 3:
+            select = raw_input("Delete Todo at number:  ")
+            my_todos.delete_todo(select)
+        if select == 4:
+            select = raw_input("Mark as complete at number:  ")
+            my_todos.complete_todo(select)
+        if select == 5:
+            select = raw_input("Delete all complete? 1. Yes 2. No  ")
+            my_todos.delete_complete_todo()
+        if select == 6:
+            pprint(my_todos.show_todo())
+        if select == 0:
+            quit()
+        else:
+            print("You have choosen, poorly.")
+    except ValueError: 
+        print("Mighty hero!! You have discovered my secrets! To claim your prize, enter a number between 1 and 6")
+        
     
