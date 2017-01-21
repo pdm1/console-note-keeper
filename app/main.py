696ce900630cf88a1b1d7b22c3032d6c6767d706
Patrick Matthies <coding.newb80@gmail.com>
2017-01-21 00:04:33 +0000

    Cleaned up the console output a little bit.

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ while my_login.sign_in(user,pw) is False:
 my_todos = Todo(user) 
 select = -1
 while select != 0:
-    print("1. Add Todo" + "\n" +
+    print( "\n" + "1. Add Todo" + "\n" +
         "2. Update Todo"  + "\n" +
         "3. Delete Todo" + "\n" +
         "4. Complete Todo" + "\n" +
@@ -29,21 +29,21 @@ while select != 0:
         "6. View all Todo" + "\n" +
         "0. Quit")
     try:
-        select = int(raw_input("choose option "))
+        select = int(raw_input( "\n" + "Choose option: "))
         if select == 1:
-            select = raw_input("Add Todo:  ")
+            select = raw_input("Add Todo: ")
             my_todos.add_todo(select)
         elif select == 2:
-            select = raw_input("Update Todo at number:  ")
+            select = raw_input("Update Todo at number: ")
             my_todos.update_todo(select)
         elif select == 3:
-            select = raw_input("Delete Todo at number:  ")
+            select = raw_input("Delete Todo at number: ")
             my_todos.delete_todo(select)
         elif select == 4:
-            select = raw_input("Mark as complete at number:  ")
+            select = raw_input("Mark as complete at number: ")
             my_todos.complete_todo(select)
         elif select == 5:
-            select = raw_input("Delete all complete? 1. Yes 2. No  ")
+            select = raw_input("Delete all complete? 1. Yes 2. No: ")
             my_todos.delete_complete_todo()
         elif select == 6:
             pprint(my_todos.show_todo())
